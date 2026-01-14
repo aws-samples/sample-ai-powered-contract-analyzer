e74b342aa27aaa839c5dad96fab504e8dedbb446
Vamsi Pitta <pitta.vamsi619@outlook.com>
2026-01-14 13:52:05 -0800

    Added .gitignore at the parent folder level

--- frontend/src/aws-config.js
+++ frontend/src/aws-config.js
@@ -2,24 +2,24 @@
 export const awsConfig = {
   Auth: {
     Cognito: {
-      userPoolId: 'us-west-2_uRwcPL4Zq',
-      userPoolClientId: 'o7pr9hg1hj8gj6sasqiumh0n4',
-      identityPoolId: 'us-west-2:341eb9d3-4ceb-41cb-875c-2394e4ae1972',
-      region: 'us-west-2'
+      userPoolId: 'YOUR_USER_POOL_ID',
+      userPoolClientId: 'YOUR_USER_POOL_CLIENT_ID',
+      identityPoolId: 'YOUR_IDENTITY_POOL_ID',
+      region: 'YOUR_AWS_REGION'
     }
   },
   API: {
     REST: {
       ContractAnalyzer: {
-        endpoint: 'https://ke9atav445.execute-api.us-west-2.amazonaws.com/prod/',
-        region: 'us-west-2'
+        endpoint: 'YOUR_API_GATEWAY_ENDPOINT',
+        region: 'YOUR_AWS_REGION'
       }
     }
   },
   Storage: {
     S3: {
-      bucket: 'contractanalyzerstack-contractsbucketf9ac583d-tuacrmwrxcbp',
-      region: 'us-west-2'
+      bucket: 'YOUR_S3_BUCKET_NAME',
+      region: 'YOUR_AWS_REGION'
     }
   }
 };
